0fd0d1de9731d6d07e09f24ee672a0f3d420bff1
evandroflores <eof@eof.com.br>
2017-12-27 17:17:03 +0000

    Logging...

--- SentryToolbar/SentryAPI.swift
+++ SentryToolbar/SentryAPI.swift
@@ -14,7 +14,9 @@ class SentryAPI {
         NSLog("SentryAPI.fetch initialized...")
         
         for organization in conf.organizations {
+            NSLog("SentryAPI.fetch Organization [\(organization.slug)]")
             for project in organization.projects {
+                NSLog("SentryAPI.fetch Organization [\(organization.slug)] Project [\(project.slug)]")
                 let session = URLSession.shared
                 let (url, token) = conf.getIssueEndpoint(organization: organization, project: project)
         
@@ -25,7 +27,7 @@ class SentryAPI {
 
                 let task = session.dataTask(with: request) { data, response, err in
                     if let error = err {
-                        NSLog("API Error URL[\(url)] Token[\(token)] :\(error)")
+                        NSLog("API Error URL[\(url)] Token[\(token)] Error[\(error)]")
                     }
                     var totalIssuesCount = Int64(-1)
                     
@@ -37,10 +39,10 @@ class SentryAPI {
                                 let issues = try decoder.decode([Issue].self, from: data!)
                                 
                                 for issue in issues {
-                                    NSLog("Issue [\(issue.title)] Total [\(issue.count)]")
+                                    NSLog("Issue[\(issue.title)] Total[\(issue.count)]")
                                     totalIssuesCount += Int64(issue.count)!
                                 }
-                                NSLog("Organization [\(organization.slug)] Project [\(project.slug)] TotalIssues [\(totalIssuesCount)]")
+                                NSLog("Organization[\(organization.slug)] Project[\(project.slug)] TotalIssues[\(totalIssuesCount)]")
                                 // TODO: API Link pagination
                             } catch {
                                 NSLog("Error trying to parse Json URL[\(url)] Token[\(token)] Error[\(error)]")
@@ -50,7 +52,7 @@ class SentryAPI {
                         case 401:
                             NSLog("Unauthorized access for: URL[\(url)] Token[\(token)]")
                         default:
-                            NSLog("API Response: [\(httpResponse.statusCode)] for URL[\(url)] Error[\(HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))]")
+                            NSLog("API Response[\(httpResponse.statusCode)] for URL[\(url)] Error[\(HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))]")
                         }
                     }
                     completion(totalIssuesCount)
