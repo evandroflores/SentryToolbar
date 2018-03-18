c240dba478c16c55c180bea0189b1000daefb307
Evandro Flores <eof@eof.com.br>
2018-03-18 21:02:10 +0000

    Adding timer to the correct place

--- SentryToolbar/Controller/SchedulerController.swift
+++ SentryToolbar/Controller/SchedulerController.swift
@@ -10,22 +10,40 @@ import Foundation
 
 class SchedulerController: NSObject {
     var conf: Config
+    var sentryApi: SentryAPI
+    var timer: Timer!
 
     override init(){
         NSLog("SchedulerController.init")
         conf = Config.configInstance
+        sentryApi = SentryAPI()
+        super.init()
+        timer = Timer.scheduledTimer(timeInterval: Config.LOOP_CYCLE_SECONDS, target: self, selector: #selector(loop), userInfo: nil, repeats: true)
     }
 
-    func start(){
+    @objc func loop(){
         for (_, org) in conf.organizations {
             NSLog("SchedulerController.loop Organization [\(org.slug)]")
             for (_, proj) in org.projects {
                 NSLog("SchedulerController.loop Organization [\(org.slug)] Project [\(proj.slug)]")
+
+                DispatchQueue.global(qos: DispatchQoS.background.qosClass).async {
+                    self.sentryApi.fetchIssues(org: org, proj: proj)
+                }
             }
         }
     }
 
+    func start(){
+        timer.fire()
+    }
+
+    func updateTotal(){
+        NSLog("SchedulerController.update")
+    }
+
     func stop(){
         NSLog("SchedulerController.stop")
+        timer.invalidate()
     }
 }
